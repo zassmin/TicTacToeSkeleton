9ad799fb5356d4d0eb34e69ee96b1804adbcbad9
zmontesd <zmontesd@gmail.com>
2013-03-10 09:57:02 -0700

    created a tictactoe app for Ruby Tuesdays

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TicTacToe::Application.config.secret_token = '54d37b936b4168e1e9ff66f076084af548b929198a3505b47a4e9362095f80475b7a1aafd8413b53d2a0a1afdd40ed8055a305613e4a676d80758ef7a65a91cd'
