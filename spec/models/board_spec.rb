ebd29fc441232dcffbe68c52a0e99ccd0de7a9b5
zmontesd <zmontesd@gmail.com>
2013-03-24 12:50:23 -0700

    removed comments

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -12,11 +12,3 @@ describe Board do
 									 [nil, nil, nil]] 
 	end
 end
-
-	# rather than nil, i should test empty? or that there is an array! yes!!!
-	# check out the data structures from connectfour
-	# not quite helpful
-	# just do it girl!
-	# check that attributes for it exist
-	# at which point do I migrate to the database?
-	# jsut do this Z!
