e60183da87f6df5152e96c31aa5b9ebbae338649
zmontesd <zmontesd@gmail.com>
2013-03-24 18:12:46 -0700

    added more tests to the board model

--- app/models/board.rb
+++ app/models/board.rb
@@ -8,4 +8,11 @@ class Board
 				  [nil, nil, nil], 
 				  [nil, nil, nil]]
 	end
+	# assigning nil gives it the nilclass, so << doesn't work on the array
+	# TODO: change the array values... 
+
+	def player_position(position, first_element, second_element)
+		@board[first_element][second_element] = position
+	end
+
 end
--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -11,4 +11,19 @@ describe Board do
 									 [nil, nil, nil], 
 									 [nil, nil, nil]] 
 	end
+
+	it "should always have a length of 3" do 
+		@show_board.board.length == 3
+	end
+
+	it "should always have the array within the array length of 3" do
+		@show_board.board.each { |array| array.length } == 3		
+	end 
+
+	describe "player_position" do 
+		it "should establish a position on the board, using 'x' or 'o'" do
+			@show_board.player_position('x', 0, 1) == @show_board.board[0][2]
+		end
+	end
+
 end
