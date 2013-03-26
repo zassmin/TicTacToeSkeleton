6deee593d03150b756980e3cf65fd3289b717001
zmontesd <zmontesd@gmail.com>
2013-03-25 20:54:34 -0700

    changed player_position method to assign_player_position and rewrote some test for it. Also updated tests for array within array length, to actually test the length within the array. Created and tested display_element and display_board methods

--- app/models/board.rb
+++ app/models/board.rb
@@ -8,11 +8,26 @@ class Board
 				  [nil, nil, nil], 
 				  [nil, nil, nil]]
 	end
-	# assigning nil gives it the nilclass, so << doesn't work on the array
-	# TODO: change the array values... 
 
-	def player_position(position, first_element, second_element)
-		@board[first_element][second_element] = position
+	def assign_player_position(player, row, column)
+		@board[row][column] = player
+	end
+
+	def display_board
+		"#{display_element(0,0)}|#{display_element(0,1)}|#{display_element(0,2)}\n" +
+	    "- - -\n" +
+		"#{display_element(1,0)}|#{display_element(1,1)}|#{display_element(1,2)}\n" +
+		"- - -\n" +
+		"#{display_element(2,0)}|#{display_element(2,1)}|#{display_element(2,2)}\n"
+	end
+
+	def display_element(row, column)
+		element = @board[row][column]
+		if element
+			element
+		else
+			' '
+		end
 	end
 
 end
--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -13,17 +13,46 @@ describe Board do
 	end
 
 	it "should always have a length of 3" do 
-		@show_board.board.length == 3
+		@show_board.board.length.should == 3
 	end
 
 	it "should always have the array within the array length of 3" do
-		@show_board.board.each { |array| array.length } == 3		
+		@show_board.board.each { |array| array.length.should == 3 } 		
 	end 
 
-	describe "player_position" do 
+	describe "assign_player_position" do 
 		it "should establish a position on the board, using 'x' or 'o'" do
-			@show_board.player_position('x', 0, 1) == @show_board.board[0][2]
+			to_test = @show_board.assign_player_position('x', 0, 1)
+			to_test.should == @show_board.board[0][1]
 		end
+	end
+
+	describe "display_element" do
+		it "should display @board element" do
+			mock_player = @show_board.assign_player_position('x', 0, 1)
+
+			to_test = @show_board.display_element(0,1)
+			to_test.should == mock_player
+		end
+
+		it "should display a space if the element is nil" do
+			@show_board.display_element(0,0).should == ' '
+		end
+
+		it "should display an 'x' or an 'o' if the element is not a space" do
+		  @show_board.assign_player_position('x', 0, 1)
+		  @show_board.display_element(0,1).should == 'x'
+		end
+	end
+
+    describe "display_board" do 
+	 	it "should display board" do 
+	 		to_test = @show_board.display_board.should == " | | \n" + 
+	 													  "- - -\n" +
+	 													  " | | \n" +
+      													  "- - -\n" +
+	 													  " | | \n"
+	 	end
 	end
 
 end
