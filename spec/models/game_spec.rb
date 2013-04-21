254d66820b3937f3c5f7e60c413a69f4b13c40d0
zmontesd <zmontesd@gmail.com>
2013-04-20 20:01:37 -0700

    move display board methods and tests to helper files

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'game'
+require 'games_helper'
 
 describe Game do 
 	before do
@@ -76,66 +77,6 @@ describe Game do
 			@test_game.play(1,1)
 			@test_game.play(0,2)
 			@test_game.play(1,2).should == "Player x is the winner!"
-		end
-	end
-
-	describe "display_element" do
-		it "should display @board element" do
-			@test_game.update_board('x', 0, 1)
-
-			@test_game.display_element(0,1).should == 'x'
-		end
-
-		it "should display a space if the element is nil" do
-			@test_game.display_element(0,0).should == ' '
-		end
-
-		it "should display an 'x' or an 'o' if the element is not a space" do
-		  @test_game.update_board('x', 0, 1)
-		  @test_game.display_element(0,1).should == 'x'
-		end
-	end
-
-	describe "display_line" do 
-		it "should display player marks or a space (if nil) in the designated row" do 
-			@test_game.update_board('x', 0, 2)
-
-			to_test = @test_game.display_line(0)
-			to_test.should == " | |x\n"
-		end
-	end
-
-  describe "display_board" do 
-	 	it "should display board" do 
-	 		@test_game.display_board.should == " | | \n" + 
-	 										   								 "- - -\n" +
-	 										   								 " | | \n" +
-      										   						 "- - -\n" +
-	 										   								 " | | \n"
-		end
-
-  	it "should display player marks on the board" do
-   		@test_game.update_board('x', 0, 0)
- 			@test_game.update_board('o', 1, 1)
- 			@test_game.update_board('x', 2, 2)
-
-   		@test_game.display_board.should == "x| | \n" + 
-	 										   								 "- - -\n" +
-	 									   	   							 " |o| \n" +
-      										   						 "- - -\n" +
-	 										                   " | |x\n"
-	 	end
-
-  	it "should display player marks on the board" do
-  		@test_game.update_board('x', 0, 0)
-  		@test_game.update_board('o', 1, 1)
-  		@test_game.update_board('x', 2, 2)
-
-  		@test_game.display_board.should == "x| | \n" + 
- 										   									 "- - -\n" +
- 									   	  								 " |o| \n" +
-    										  							 "- - -\n" +
- 										  									 " | |x\n"
 		end
 	end
 
