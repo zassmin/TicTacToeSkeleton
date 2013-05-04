d767aa9427cd090a3ef570401542e126951e9bb1
zmontesd <zmontesd@gmail.com>
2013-05-03 17:29:37 -0700

    clean notes, include inspect in root_path, and add game.play logic to /show (still broken)

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,47 +1,30 @@
 class GamesController < ApplicationController
 
+# start writing tests for these controllers!
+
 	def index
 		@games = Game.all
 	end
+  puts @games.inspect
 
   #path /new
 	def new
 		@game = Game.new
     redirect_to show_path, :status => 301
 	end
-
-  # Shows game with given :id
-  # @verb MATCH
-  # @path /:id
+  puts @game.inspect
+ 
   def show
     if Game.exists?(params[:id])
       @game = Game.find(params[:id])
-    else
-      render :status => 404
     end
   end
   
-  # Creates a game recourse
-  #
-  # @verb POST
-  # @path /games
-  # @return :success => true/false
-  #         :id      => id of new game
-  #         :game    => new game object
   def create
      @game = Game.create!
-     render :success => true,
-            :id => @game.id,
-            :game => @game,
-            :status => 201
   end
 
   def destroy
-    if Game.exists?(params[:id]) && Game.destroy(params[:id])
-      render :success => true, :status => 200
-    else
-      render :success => false, :status => 404
-    end
   end
 
   def play
